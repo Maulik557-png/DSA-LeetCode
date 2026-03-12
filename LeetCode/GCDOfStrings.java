9070417c14f5dd15cccdac8b060b548c6fc5d3e7
smaulik557-admin <smaulik557@gmail.com>
2026-03-12 14:40:40 +0530

    Greatest common divisor in Strings

---
+++ LeetCode/GCDOfStrings.java
@@ -0,0 +1,18 @@
+public class GCDOfStrings {
+    public static int gcd(int a, int b)    {
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+    public static String gcdOfStrings(String str1, String str2) {
+        if((str1 + str2).equals(str2 + str1)) {
+            return str1.substring(0, gcd(str1.length(), str2.length()));
+        }
+        else return "";
+    }
+    
+    public static void main(String[] args)  {
+        String s1 = "ABCABC";
+        String s2 = "ABC";
+        System.out.println(gcdOfStrings(s1, s2));
+    }
+}

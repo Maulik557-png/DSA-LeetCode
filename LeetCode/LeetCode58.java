8517a01aa77e0dc026683f115ca02233d0cb8d23
smaulik557-admin <smaulik557@gmail.com>
2026-03-15 12:00:18 +0530

    LeetCode Problems

---
+++ LeetCode/LeetCode58.java
@@ -0,0 +1,62 @@
+public class LeetCode58 {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+    }
+
+    // public static int lengthOfLastWord(String s) {
+
+    // String newString = s.strip();
+    // char space = ' ';
+
+    // int counter = -1;
+
+    // for (int i = 0; i < newString.length(); i++) {
+    // if(newString.charAt(i) == space) {
+    // counter = i;
+    // }
+    // }
+
+    // String sub = newString.substring(counter + 1);
+
+    // return sub.length();
+    // }
+
+    // public static int lengthOfLastWord(String s) {
+
+    // String trimmedString = s.strip();
+    // StringBuilder sb = new StringBuilder(trimmedString);
+    // String reversedString = sb.reverse().toString();
+
+    // char space = ' ';
+    // int counter = 0;
+
+    // for (int i = 0; i < reversedString.length(); i++) {
+    // if(reversedString.charAt(i) != space) {
+    // counter++;
+    // }
+    // else break;
+    // }
+
+    // return counter;
+    // }
+
+    // public static int lengthOfLastWord(String s) {
+    //     String str = new StringBuilder(s.strip()).reverse().toString();
+    //     int count = 0;
+
+    //     for (int i = 0; i < str.length(); i++) {
+    //         if (str.charAt(i) != ' ')
+    //             count++;
+    //         else
+    //             break;
+    //     }
+
+    //     return count;
+    // }
+
+    public static int lengthOfLastWord(String s) {
+        s = s.trim();
+        return s.length() - s.lastIndexOf(' ') - 1;
+    }
+}
---
+++ LeetCode/LeetCode76.java
@@ -0,0 +1,50 @@
+public class LeetCode76 {
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+
+        System.out.println(minWindow(s, t));
+    }
+
+    public static String minWindow(String s, String t) {
+        if (s.length() < t.length())  return "";
+
+        int[] target = new int[128];
+        int[] window = new int[128];
+
+        for (char c : t.toCharArray()) {
+            target[c]++;
+        }
+
+        int left = 0;
+        int count = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            window[c]++;
+
+            if (window[c] <= target[c]) {
+                count++;
+            }
+            while (count == t.length()) {
+
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+                window[leftChar]--;
+
+                if (window[leftChar] < target[leftChar]) {
+                    count--;
+                }
+
+                left++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+}

313b53c17c80743194f90dda874248de73c7390c
smaulik557-admin <smaulik557@gmail.com>
2026-03-22 14:24:26 +0530

    Day 7: Understanding LeetCode Patterns

---
+++ LeetCode/LeetCode1679.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class LeetCode1679 {
+
+    // public static int maxOperations(int[] nums, int k) {
+
+    //     int ops = 0;
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if(nums[i] < k && nums[i] != 0) {
+    //             int var = i;
+    //             for (int j = i + 1; j < nums.length; j++) {
+    //                 if (((k - nums[i]) == nums[j]) && nums[j] < k && nums[j] != 0) {
+    //                     nums[i] = 0;
+    //                     nums[j] = 0;
+    //                     var++;
+    //                     ops++;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return ops;
+    // }
+
+    public static int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1, ops = 0;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == k) {
+                ops++;
+                left++;
+                right--;
+            } else if (sum < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ops;
+    }
+
+    public static void main(String[] args) {
+        
+        int[] nums = {1,2,3,4};
+        int k = 5;
+
+        System.out.println(maxOperations(nums, k));
+
+    }
+}
---
+++ LeetCode/LeetCode2090.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class LeetCode2090   {
+
+    public static void main(String[] args) {
+        int[] nums = {40527,53696,10730,66491,62141,83909,78635,18560};
+        int k = 2;
+        int[] res = getAverages(nums, k);
+
+        for (int i : res) {
+            System.out.println(i);
+        }
+    }
+
+    private static int[] getAverages(int[] nums, int k) {
+
+        int[] res = new int[nums.length];
+        Arrays.fill(res, -1);
+
+        if(k == 0)  return nums;
+
+        if(k > nums.length / 2)  return res;
+
+        int l = 0;
+        int r = 2 * k;
+
+        int pivot = k;
+
+        while(r < nums.length)  {
+
+            int avg = 0;
+            
+            for(int i = l; i <= r; i++) {
+                avg += nums[i];
+            }
+            res[pivot] = avg / (2 * k + 1);
+
+            r++;
+            l++;
+            pivot++;
+        }
+        return res;
+    }
+}

0a4b8c9d28b9833c81ae1cc35fcd3d823ceef8ee
smaulik557-admin <smaulik557@gmail.com>
2026-03-27 22:45:06 +0530

    Day 12: Understanding LeetCode Patterns

---
+++ LeetCode/LeetCode136.java
@@ -0,0 +1,40 @@
+package HashMap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode136 {
+    public static void main(String[] args) {
+        int[] nums = {4,1,2,1,2};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+        // Set<Integer> set = new HashSet<>();
+        // int sumSet = 0;
+        // int sumArray = 0;
+        // for (int n : nums) {
+        //     if (!set.contains(n)) {
+        //         set.add(n);
+        //         sumSet += n;
+        //     }
+        //     sumArray += n;
+        // }
+        // return 2 * sumSet - sumArray;
+
+        // int res = 0;
+        // for (int n : nums)  {
+        //     res ^= n;
+        // }
+        // return res;
+
+        Set<Integer> set = new HashSet<>();
+        int sumSet = 0;
+        for (int n : nums) {
+            sumSet += n;
+            if (set.contains(n))  sumSet -= 2 * n;
+            set.add(n);
+        }
+        return sumSet;
+    }
+}

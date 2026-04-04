a97d63cf0a5c3f50113a0d53d591066de2ae007e
smaulik557-admin <smaulik557@gmail.com>
2026-04-04 14:23:22 +0530

    Day 20: Understanding LeetCode Patterns

---
+++ LeetCode/LeetCode224.java
@@ -0,0 +1,92 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class LeetCode224 {
+
+    public static void main(String[] args) {
+        String s = "1 + 1";
+        System.out.println(calculate(s));
+    }
+
+    public static int calculate(String s) {
+        s = s.strip();
+        int num = 0;
+        boolean hasNum = false;
+        Stack<Integer> nums = new Stack<>();
+        Stack<Character> opr = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == ' ')
+                continue;
+            if (c >= '0' && c <= '9') {
+                num = num * 10 + (c - '0');
+                hasNum = true;
+            } else if (c == '(') {
+                opr.push(c);
+            } else if (c == ')') {
+                if (hasNum) {
+                    nums.push(num);
+                    num = 0;
+                    hasNum = false;
+                }
+                while (opr.peek() != '(') {
+                    char op = opr.pop();
+                    int n2 = nums.pop();
+                    int n1 = nums.pop();
+                    nums.push(performOps(n1, n2, op));
+                }
+                opr.pop();
+            } else {
+                int j = i - 1;
+                while (j >= 0 && s.charAt(j) == ' ')  j--;
+                if (c == '-' && (j < 0 || s.charAt(j) == '('))  nums.push(0);
+
+                if (hasNum) {
+                    nums.push(num);
+                    num = 0;
+                    hasNum = false;
+                }
+                while (!opr.isEmpty() && opr.peek() != '(' &&
+                        precedence(opr.peek()) >= precedence(c)) {
+
+                    char op = opr.pop();
+                    int n2 = nums.pop();
+                    int n1 = nums.pop();
+                    nums.push(performOps(n1, n2, op));
+                }
+                opr.push(c);
+            }
+        }
+
+        if (hasNum) {
+            nums.push(num);
+        }
+        while (!opr.isEmpty()) {
+            char op = opr.pop();
+            int n2 = nums.pop();
+            int n1 = nums.pop();
+            nums.push(performOps(n1, n2, op));
+        }
+        return nums.peek();
+    }
+
+    private static int precedence(char op) {
+        if (op == '+' || op == '-')
+            return 1;
+        return 0;
+    }
+
+    private static int performOps(int n1, int n2, char operator) {
+        switch (operator) {
+            case '+':
+                return n1 + n2;
+            case '-':
+                return n1 - n2;
+            default:
+                return 0;
+        }
+    }
+}
---
+++ LeetCode/LeetCode2248.java
@@ -0,0 +1,78 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LeetCode2248 {
+    public static void main(String[] args) {
+        int[][] nums = {{1,2,3},{3,4,5,6}};
+        List<Integer> list = intersection(nums);
+
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+    }
+
+    public static List<Integer> intersection(int[][] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        int[] fArr = nums[0];
+
+        for (int[] arr: nums) {
+            for (int n: arr) {
+                map.put(n, map.getOrDefault(n, 0) + 1);
+            }
+        }
+
+        for (int i = 0; i < fArr.length; i++) {
+            if(map.get(fArr[i]) == nums.length)  {
+                res.add(fArr[i]);
+            }
+        }
+
+        res.sort((a, b) -> a - b);
+        return res;
+    }
+
+    // public static List<Integer> intersection(int[][] nums) {
+    //     Map<Integer, Integer> map = new HashMap<>();
+    //     List<Integer> res = new ArrayList<>();
+    //     for (int[] arr : nums)
+    //         for (int n : arr)
+    //             map.put(n, map.getOrDefault(n, 0) + 1);
+    //     return map.keySet().stream().filter(i -> map.get(i) == nums.length).sorted().collect(Collectors.toList());
+    // }
+
+    // public static List<Integer> intersection(int[][] nums) {
+    //     Map<Integer, Integer> map = new HashMap<>();
+
+    //     for (int[] arr : nums) {
+    //         Set<Integer> set = new HashSet<>();
+    //         for (int n : arr)  set.add(n);
+    //         for (int n : set)  map.put(n, map.getOrDefault(n, 0) + 1);
+    //     }
+
+    //     return map.keySet()
+    //             .stream()
+    //             .filter(i -> map.get(i) == nums.length)
+    //             .sorted()
+    //             .collect(Collectors.toList());
+    // }
+
+    // public List<Integer> intersection(int[][] nums) {
+    //     Set<Integer> res = new HashSet<>();
+
+    //     for (int n : nums[0])  res.add(n);
+    //     for (int i = 1; i < nums.length; i++) {
+    //         Set<Integer> temp = new HashSet<>();
+    //         for (int n : nums[i])  temp.add(n);
+    //         res.retainAll(temp);
+    //     }
+
+    //     List<Integer> ans = new ArrayList<>(res);
+    //     Collections.sort(ans);
+    //     return ans;
+    // }
+}

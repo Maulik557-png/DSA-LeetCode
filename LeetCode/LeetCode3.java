5a6899edefaaf341a099637174fcc942e4443201
smaulik557-admin <smaulik557@gmail.com>
2026-04-06 17:52:25 +0530

    Feat: Added one more approach for LeetCode 3

--- LeetCode/LeetCode3.java
+++ LeetCode/LeetCode3.java
@@ -1,30 +1,60 @@
-package LeetCode;
-
-import java.util.HashSet;
-import java.util.Set;
+// import java.util.HashSet;
+// import java.util.Set;
 
 public class LeetCode3 {
     public static void main(String[] args) {
         String s = "abcabcbb";
         System.out.println(lengthOfLongestSubstring(s));
-
     }
 
     public static int lengthOfLongestSubstring(String s) {
+        int[] index = new int[128]; 
+        int res = 0;
+        int l = 0;
 
-        Set<Character> set = new HashSet<>();
-        int left = 0;
-        int maxLen = 0;
+        for (int r = 0; r < s.length(); r++) {
+            char ch = s.charAt(r);
+            index[ch]++;
 
-        for (int right = 0; right < s.length(); right++) {
-            while (set.contains(s.charAt(right))) {
-                set.remove(s.charAt(left));
-                left++;
+            while(index[ch] > 1) {
+                index[s.charAt(l)]--;
+                l++;
             }
-
-            set.add(s.charAt(right));
-            maxLen = Math.max(maxLen, right - left + 1);
+            res = Math.max(res, r - l + 1);
         }
-        return maxLen;
+        return res;
     }
+
+    // public static int lengthOfLongestSubstring(String s) {
+
+    //     Set<Character> set = new HashSet<>();
+    //     int left = 0;
+    //     int maxLen = 0;
+
+    //     for (int right = 0; right < s.length(); right++) {
+    //         while (set.contains(s.charAt(right))) {
+    //             set.remove(s.charAt(left));
+    //             left++;
+    //         }
+
+    //         set.add(s.charAt(right));
+    //         maxLen = Math.max(maxLen, right - left + 1);
+    //     }
+    //     return maxLen;
+    // }
+
+    // public static int lengthOfLongestSubstring(String s) {
+    //     int[] index = new int[128]; 
+    //     int maxLen = 0;
+    //     int left = 0;
+
+    //     for (int i = 0; i < s.length(); i++) {
+    //         char ch = s.charAt(i);
+
+    //         left = Math.max(left, index[ch]);
+    //         maxLen = Math.max(maxLen, i - left + 1);
+    //         index[ch] = i + 1;
+    //     }
+    //     return maxLen;
+    // }
 }

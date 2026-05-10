74a764a598965b2ef16369027d9a1f14b2fe0105
smaulik557-admin <smaulik557@gmail.com>
2026-05-10 12:53:21 +0530

    Day 55: Understanding LeetCode Patterns

---
+++ LeetCode/LeetCode33.java
@@ -0,0 +1,76 @@
+public class LeetCode33 {
+    public static void main(String[] args) {
+        int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 0;
+        System.out.println(search(nums, target));
+    }
+
+    public static int search(int[] nums, int k) {
+
+        int l = 0;
+        int r = nums.length - 1;
+        int mid = (l + r) / 2;
+
+        if(nums[mid] == k) return mid;
+
+        int nl = mid - 1;
+        int nr = mid + 1;
+
+        while(nums[mid] != k) {
+
+        }
+
+
+
+
+
+
+
+        return -1;
+    }
+
+    // find the smallest index such that nums[i] >= k
+    // Basically the first occurence of any number
+    public static int lowerBound(int[] nums, int k) {
+        int n = nums.length;
+        int res = n;
+
+        int l = 0;
+        int r = n - 1;
+        
+        while(l <= r) {
+            
+            int mid = (l + r) / 2;
+
+            if(nums[mid] >= k) {
+                res = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    // find the smallest index such that nums[i] > k
+    // basically the last occurence of any number + 1
+    public static int upperBound(int[] nums, int k) {
+        int n = nums.length;
+        int res = n;
+        int l = 0;
+        int r = n - 1;
+        while(l <= r) {
+            
+            int mid = (l + r) / 2;
+
+            if(nums[mid] > k) {
+                res = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return res;
+    }
+
+}

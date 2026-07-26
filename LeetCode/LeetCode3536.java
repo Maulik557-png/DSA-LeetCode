0d5bf0c9d76c32c7c749fe2f3429f8c329fae020
smaulik557-admin <smaulik557@gmail.com>
2026-07-26 11:25:59 +0530

    Day 119: Understanding LeetCode Patterns

---
+++ LeetCode/LeetCode3536.java
@@ -0,0 +1,19 @@
+public class LeetCode3536 {
+    public int maxProduct(int n) {
+        int a = 0;
+        int b = 0;
+
+        while (n > 0) {
+            int d = n % 10;
+            if (d > a) {
+                b = a;
+                a = d;
+            } else if (d > b) {
+                b = d;
+            }
+            n /= 10;
+        }
+
+        return a * b;
+    }
+}

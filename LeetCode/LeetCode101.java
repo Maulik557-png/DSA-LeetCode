b31146542679ce0732c09ea6b26ed658e8c9c423
smaulik557-admin <smaulik557@gmail.com>
2026-08-13 11:36:16 +0530

    Day 136: Understanding LeetCode Patterns

---
+++ LeetCode/LeetCode101.java
@@ -0,0 +1,23 @@
+public class LeetCode101 {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || symmetric(root.left, root.right);
+    }
+
+    public boolean symmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return symmetric(left.left, right.right) && symmetric(left.right, right.left);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+}

25eb95bd996a755f78baf343620a5abf0d638a5a
smaulik557-admin <smaulik557@gmail.com>
2026-09-02 13:36:46 +0530

    September 2 POTD

---
+++ LeetCode/0926POTDs/Sep02P3875.java
@@ -0,0 +1,8 @@
+public class Sep02P3875 {
+    // If all are odd -> true
+    // If all are even -> true
+    // If odd + even, we can always convert it to odd -> true
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}

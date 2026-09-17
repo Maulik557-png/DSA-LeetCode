faf7564b6632a82400178d75662d3d27db5c6446
smaulik557-admin <smaulik557@gmail.com>
2026-09-17 08:53:04 +0530

    Day 166: Understanding LeetCode Patterns

---
+++ LeetCode/LeetCode836.java
@@ -0,0 +1,13 @@
+public class LeetCode836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int[] x1 = { rec1[0], rec1[2] };
+        int[] y1 = { rec1[1], rec1[3] };
+        int[] x2 = { rec2[0], rec2[2] };
+        int[] y2 = { rec2[1], rec2[3] };
+
+        boolean x = x2[0] < x1[1] && x1[0] < x2[1];
+        boolean y = y2[0] < y1[1] && y1[0] < y2[1];
+
+        return x && y;
+    }
+}
